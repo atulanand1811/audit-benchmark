e412d4e297582cf6282302de0eba253dcee2a9c3
atulanand1811 <61856564+atulanand1811@users.noreply.github.com>
2021-08-01 16:41:20 +0530

    Update BenchmarkController.java
    cross-origin added

--- src/main/java/com/cts/benchmark/BenchmarkController.java
+++ src/main/java/com/cts/benchmark/BenchmarkController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,8 @@ import lombok.extern.slf4j.Slf4j;
 
  *
  */
+
+@CrossOrigin(origins = "*")
 @RestController
 @Slf4j
 public class BenchmarkController {
